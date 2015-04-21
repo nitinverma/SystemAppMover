0eda41db0ca76970991bf78440240b90ea88a007
Nitin Verma <nitin.verma@inmobi.com>
2015-04-21 15:40:32 +0530

    Using PrintWriter

--- src/main/java/de/j4velin/systemappmover/Logger.java
+++ src/main/java/de/j4velin/systemappmover/Logger.java
@@ -19,38 +19,54 @@ package de.j4velin.systemappmover;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.Date;
 
 import android.os.Environment;
+import android.util.Log;
 
 public class Logger {
 
-    private static FileWriter fw;
+    private static PrintWriter fw;
     private static Date date = new Date();
     private final static String APP = "SystemAppMover";
 
+    static {
+        final File appLogFile = new File(Environment.getExternalStorageDirectory(), APP + ".log");
+        if (fw == null) {
+            try {
+                fw = new PrintWriter(new FileWriter(appLogFile, true));
+                Log.i(APP, "sending app logs to " + appLogFile.getAbsolutePath());
+            } catch (IOException e) {
+                Log.e(APP, "Could not create file writer", e);
+            }
+        }
+    }
+
     public static void log(Throwable ex) {
+        ex.printStackTrace(fw);
+        ex.printStackTrace(System.err);
         log(ex.getMessage());
         for (StackTraceElement ste : ex.getStackTrace()) {
             log(ste.toString());
         }
     }
 
-    @SuppressWarnings("deprecation")
+    //@SuppressWarnings("deprecation")
     public static void log(String msg) {
         if (!BuildConfig.DEBUG)
             return;
+        if (msg == null) {
+            msg = "null";
+        }
         android.util.Log.d(APP, msg);
         try {
-            if (fw == null) {
-                fw = new FileWriter(new File(Environment.getExternalStorageDirectory().toString() + "/" + APP + ".log"), true);
-            }
             date.setTime(System.currentTimeMillis());
             fw.write(date.toLocaleString() + " - " + msg + "\n");
+        } finally {
             fw.flush();
-        } catch (IOException e) {
-            e.printStackTrace();
         }
+
     }
 
     protected void finalize() throws Throwable {
